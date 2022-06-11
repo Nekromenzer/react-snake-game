2af37aaf66407504cda2e48700afe89b95ddd72a
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-06-12 02:36:40 +0530

    random foods

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react'
+
 interface FoodProps {
-    dot: number[]
+    dot: number[],
+    score:number
 }
 
-export default function Food(props: FoodProps) {
+function Food(props: FoodProps) {
+    const [currentFood , setCurrentFood] = React.useState("");
+    const foodClassNameArray = ['food-1','food-2','food-3','food-4'];
+
+    const randomFoodClassName = ()=>{
+        setCurrentFood(foodClassNameArray[Math.floor(Math.random() * foodClassNameArray.length)]);
+    }
+
+    React.useEffect(() => {
+        randomFoodClassName();
+    }, [props.score])
     const styleFood = {
         left: `${props.dot[0]}%`,
         top: `${props.dot[1]}%`
     }
     return (
-        <div className="snake-food animate-bounce " style={styleFood}>
+        <div className={`snake-food animate-bounce ${currentFood}`} style={styleFood}>
         </div>
     )
 }
+
+export default React.memo(Food);
