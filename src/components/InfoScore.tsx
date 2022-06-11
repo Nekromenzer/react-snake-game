80a53a652877a75d4b59cd4d4b5f5df0396357b7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-06-12 00:40:01 +0530

    scrore ui

--- src/components/InfoScore.tsx
+++ src/components/InfoScore.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import useLocalStorage from "../hooks/useLocalStorage"
 
 interface InfoScoreProps {
-    score: number
+    score: number,
+    isPause: boolean,
 }
 
 export default function InfoScore(props: InfoScoreProps) {
@@ -11,12 +12,11 @@ export default function InfoScore(props: InfoScoreProps) {
         if (props.score > bestScore) {
             setBestScore(props.score)
         }
-    },[props.score])
+    },[bestScore, props.score, setBestScore])
     return (
-        <div className="opacity-30 p-2">
-            <p className="font-bold">Current Score: {props.score}</p>
-            <p className="font-bold">Best Score: {bestScore}</p>
-
+        <div className={`${props.isPause ? 'text-yellow-200' : 'text-blue-900'} opacity-50 p-2 flex justify-between`}>
+            <span className="">Current Score : {props.score}</span>
+            <span className="">Best Score : {bestScore}</span>
         </div>
     )
 }
